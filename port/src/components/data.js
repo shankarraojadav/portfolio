da625ad69a321fda59ee0d0121abc212d323363a
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-10-13 12:19:00 +0530

    changes

--- port/src/components/data.js
+++ port/src/components/data.js
@@ -32,6 +32,25 @@ export const works = [
     github: "https://github.com/shankarraojadav/Dashboard_react",
     live: "https://react-dashboard-577q.onrender.com",
   },
+
+  {
+    card_img:
+      "https://res.cloudinary.com/dflhxdxgb/image/upload/v1697179056/retink_jznp2q.png",
+    card_title: "Retink Dashboar",
+    card_desc: "This is the Front-End challenge given by Retink",
+    github: "https://github.com/shankarraojadav/Retink.git",
+    live: "http://retinkjadhav.s3-website.ap-south-1.amazonaws.com",
+  },
+
+  {
+    card_img:
+      "https://res.cloudinary.com/dflhxdxgb/image/upload/v1697179518/three_e6xlxo.png",
+    card_title: " Three js ",
+    card_desc: "Rendering 3d model in a react app using three js",
+    github: "https://github.com/shankarraojadav/Threejs_Project.git",
+    live: "http://jadahvthreejs.s3-website.ap-south-1.amazonaws.com",
+  },
+
   {
     card_img:
       "https://res.cloudinary.com/dflhxdxgb/image/upload/v1696070154/bcpd7uiqworplmk8hiky.png",
